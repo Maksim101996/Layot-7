bc5b1ac246ef683790f827fc7f495169655ef9ae
Maksim101996 <akakii.botov@mail.com>
2023-04-20 16:02:49 +0500

    Last commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if (ScrollTrigger.isTouch !== 1) {
+	ScrollSmoother.create({
+		wrapper: ".wrapper",
+		content: ".content",
+		smooth: 1.5,
+		effects: true
+	})
+
+	gsap.fromTo(".hero-section", { opacity: 1 }, {
+		opacity: 0,
+		scrollTrigger: {
+			trigger: ".hero-section",
+			start: "center",
+			end: "820",
+			scrub: true
+		}
+	})
+
+	let itemLeft = gsap.utils.toArray(".gallery__left .gallery__item")
+	let itemRight = gsap.utils.toArray(".gallery__right .gallery__item")
+
+	itemLeft.forEach(item => {
+		gsap.fromTo(item, { x: -150, opacity: 0 }, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: "-850",
+				end: "-100",
+				scrub: true
+			}
+		})
+
+	}
+	);
+	itemRight.forEach(item => {
+		gsap.fromTo(item, { x: 200, opacity: 0 }, {
+			opacity: 1, x: 0,
+			scrollTrigger: {
+				trigger: item,
+				start: "-850",
+				end: "-100",
+				scrub: true
+			}
+		})
+
+	}
+	);
+
+}
